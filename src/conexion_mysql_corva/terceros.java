201f870c965d957033b5e445b026769a54d6c0ae
Jhon Jairo <Jhon Jairo@MrNobdy>
2018-09-27 02:54:03 -0500

    //Se implementa el metodo de consultar

--- src/conexion_mysql_corva/terceros.java
+++ src/conexion_mysql_corva/terceros.java
@@ -19,13 +19,31 @@ public class terceros extends terceroPOA {
 
     @Override
     public boolean eliminartercero(int id) {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public String consultartercero(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        String resultado = "";
+        
+            try {
+                String sqlConsultar = "Select * from terceros where id = "+ id;
+                objConec.conectar();
+                Statement st = objConec.conex.createStatement();
+                ResultSet rs = st.executeQuery(sqlConsultar);
+                    while (rs.next()) {
+                        resultado += rs.getString(2) + " - "
+                        + rs.getString(3) + " - "
+                        + rs.getString(4);
+            }
+            
+        } catch (Exception e) {
+            
+        }
+        
+        return resultado;    }
 
     @Override
     public void shoutdown() {
